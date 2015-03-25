08cccad83644e65967612c92bdd81ccb9ef25034
Kaustuv Chakrabarti <kchakrab@wks-15-22>
2015-03-25 11:23:38 +0100

    Average revision counter added

--- src/AverageRevisionCounter/AverageRevisionCounter.java
+++ src/AverageRevisionCounter/AverageRevisionCounter.java
@@ -11,6 +11,13 @@ import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import Commons.Constants;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -19,11 +26,15 @@ import org.json.JSONObject;
  */
 public class AverageRevisionCounter {
 
+    DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+    long count = 0L;
+
     public static void main(String[] args) {
 
         String inputFile = "../files/random10000Names.txt";
+//        String inputFile = "../files/ran.txt";
         String line = "";
-        String continueId = "";
+
         AverageRevisionCounter av = new AverageRevisionCounter();
 
         //String url = "https://en.wikipedia.org/w/api.php?action=query&titles=Anarchy&format=json&prop=extracts&continue&redirects&prop=revisions&rvdir=newer&rvlimit=max&rvprop=ids|flags|timestamp";
@@ -31,23 +42,45 @@ public class AverageRevisionCounter {
             BufferedReader bf = null;
             try {
                 bf = new BufferedReader(new FileReader(inputFile));
+                Date[] range = new Date[2];
+                range[0] = new Date();                  //Todays date to minimum date
+                range[1] = new Date(0L);    //Earliest date to maximum date
+                int processedPage = 0;
 
                 while ((line = bf.readLine()) != null) {
-                    URL url = av.generateUrl(line, continueId);
-                    InputStream is = url.openStream();
-                    BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-                    String jsonText = rd.readLine();
-                    
-                    continueId = av.processJson(new JSONObject(jsonText));
-                    
-                    System.out.println(jsonText);
-                    rd.close();
+                    processedPage++;
+                    String continueId = "0";
+                    try {
+                        while (!continueId.equals("-1")) {
+                            URL url = av.generateUrl(line, continueId);
+                            InputStream is = url.openStream();
+                            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                            String jsonText = rd.readLine();
+
+                            continueId = Integer.toString(av.processJson(new JSONObject(jsonText), range));
+
+                            rd.close();
+                        }
+                    } catch (Exception ex) {
+                        Logger.getLogger(AverageRevisionCounter.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    if (processedPage % 100 == 0) {
+                        System.out.println("Pages Processed :" + processedPage);
+                    }
                 }
+
+                System.out.println("Max Date: " + range[1]);
+                System.out.println("Min Date: " + range[0]);
+                System.out.println("Count: " + av.count);
+
+                av.computeAverage(range);
             } finally {
                 bf.close();
             }
         } catch (IOException ex) {
             Logger.getLogger(AverageRevisionCounter.class.getName()).log(Level.SEVERE, null, ex);
+
         }
 
     }
@@ -55,7 +88,7 @@ public class AverageRevisionCounter {
     private URL generateUrl(String title, String continueId) throws MalformedURLException {
         StringBuilder sb = new StringBuilder(Constants.WIKI_API_BASE);
         sb.append("?").append("action=").append("query");
-        sb.append("&").append("titles=").append(title);
+        sb.append("&").append("titles=").append(title.trim().replace(" ", "+"));
         sb.append("&").append("format=").append("json");
         sb.append("&").append("prop=").append("extracts");
         sb.append("&").append("continue");
@@ -63,17 +96,54 @@ public class AverageRevisionCounter {
         sb.append("&").append("prop=").append("revisions");
         sb.append("&").append("rvdir=").append("newer");
         sb.append("&").append("rvlimit=").append("max");
-        sb.append("&").append("rvprop=").append("ids|flags|timestamp");
+        sb.append("&").append("rvprop=").append("flags|timestamp");
         sb.append("&").append("rvcontinue=").append(continueId);
 
-        System.out.println("url: " + sb.toString());
-
-        return new URL(sb.toString());
+        String url = sb.toString();
+        System.out.println("url: " + url);
+        return new URL(url);
     }
 
-    private String processJson(JSONObject jo){
-        
-        
-        return null;
+    private int processJson(JSONObject jo, Date[] range) throws ParseException {
+        JSONObject pages = jo.getJSONObject("query").getJSONObject("pages");
+        Iterator<String> pageIdsIter = pages.keys();
+        String pageId = pageIdsIter.next();
+
+        if (!pages.getJSONObject(pageId).has("revisions")) {
+            return -1;
+        }
+
+        JSONArray revisions = pages.getJSONObject(pageId).getJSONArray("revisions");
+
+        count += revisions.length();
+        for (int i = 0; i < revisions.length(); i++) {
+            JSONObject rev = revisions.getJSONObject(i);
+
+            if (!rev.has("minor")) {
+                Date date = format.parse(rev.getString("timestamp").substring(0, 10));
+
+                if (date.compareTo(range[0]) < 0) {         //date is before min date replace
+                    range[0] = date;
+                }
+
+                if (date.compareTo(range[1]) > 0) {      //date is after max date replace
+                    range[1] = date;
+                }
+            }
+        }
+
+        if (!jo.has("continue")) {
+            return -1;
+        }
+
+        return jo.getJSONObject("continue").getInt("rvcontinue");
+
+    }
+
+    private void computeAverage(Date[] range) {
+        long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+        double diffInDays = ((range[1].getTime() - range[0].getTime()) / DAY_IN_MILLIS);
+
+        System.out.println("Average : " + (Double) Double.parseDouble(Long.toString(count)) / diffInDays);
     }
 }
